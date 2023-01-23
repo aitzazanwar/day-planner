f0ab6ba1b3e4ce15e017da9d3f43abebf51d759c
aitzaz <aitzaz79@gmail.com>
2023-01-23 23:19:43 +0000

    image for save btn added, local storage activated using JSON.parse

--- assets/index.js
+++ assets/index.js
@@ -16,3 +16,21 @@ $(document).ready(function() {
     $dateHeading.text(now);
 
   });
+
+    // using font awesome icon https://fontawesome.com/license
+    // change description here - none
+    const saveIcon = "save-regular.svg"; 
+  
+    // Get stored todos from localStorage
+    // Parsing the JSON string to an object
+    let storedPlans = JSON.parse(localStorage.getItem("storedPlans"));
+  
+    if (currentTime) { (storedPlans); }
+  
+    // If plans were retrieved from localStorage, update the plan array to it
+    if (storedPlans !== null) {
+      planTextArr = storedPlans;
+    } else {
+      // this should only occur on first time the app is loaded in the browser
+    
+    }
